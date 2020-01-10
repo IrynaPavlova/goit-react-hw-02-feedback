94238997a4c8275cf6c04bb77608955b3dcedad3
Pavlova <irochkapavlova85@gmail.com>
2020-01-10 15:48:36 +0200

    Updates

---
+++ precache-manifest.9a15010bca42f45d7a30b47fe5bf299d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fde70c147b203033e7dbf71f0768815",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "8f53d41fb109038bf841",
+    "url": "/goit-react-hw-02-feedback/static/js/2.23def0c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.23def0c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09fdd10af2afa4485f26",
+    "url": "/goit-react-hw-02-feedback/static/js/main.979dd20c.chunk.js"
+  },
+  {
+    "revision": "da8613172740da5a7c85",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.0a3f7794.js"
+  }
+]);
