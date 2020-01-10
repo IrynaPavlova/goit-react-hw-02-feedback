61afe43bb3066d508111d3890a9a18e1ab251d02
Pavlova <irochkapavlova85@gmail.com>
2020-01-10 15:42:03 +0200

    change homepage

---
+++ build/precache-manifest.4dba8b33fee659b09e5aa48ca437dd19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fe26cd7c2ead88c739177b0e0ce322",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "8f53d41fb109038bf841",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.23def0c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.23def0c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09fdd10af2afa4485f26",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.979dd20c.chunk.js"
+  },
+  {
+    "revision": "a22d6ae82fb1dc14f7fa",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.1ba8669c.js"
+  }
+]);
